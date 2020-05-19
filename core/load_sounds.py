7b4f80d310af5c01e0ac11d5dcdb17bf76d92143
lucasmllr <lucasmoeller@me.com>
2020-05-19 20:55:57 +0200

    fixed log(0) in specs, added saving / loading mels

--- core/load_sounds.py
+++ core/load_sounds.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 import numpy as np
 import matplotlib.pyplot as plt
 from librosa.core import load
-from librosa.feature import melspectrogram as mel
+from librosa.feature import melspectrogram as melspec
 from tqdm import tqdm
 import warnings
 import random
@@ -28,19 +28,21 @@ class SoundLoader:
         return recordings
 
     @staticmethod
-    def make_log_mels_from_recordings(recordings, sampling_rate, n_mels, fft_window, hop_length):
-        mels = [
-            np.log(mel(rec,
+    def make_log_mels_from_recordings(recordings, sampling_rate, n_mels, fft_window, hop_length, epsilon):
+        mels = []
+        for rec in recordings:
+            mel = melspec(
+                rec,
                 sr=sampling_rate,
                 n_fft=fft_window,
                 hop_length=hop_length,
                 n_mels=n_mels
-               )) for rec in recordings
-        ]
+            )
+            mels.append(np.log(mel + np.array([epsilon])))
         return mels
 
     @classmethod
-    def make_log_mels_for_classes(cls, path, classes, sampling_rate, n_mels, fft_window, hop_length):
+    def make_log_mels_for_classes(cls, path, classes, sampling_rate, n_mels, fft_window, hop_length, epsilon):
         assert os.path.isdir(path)
         all_mels = {}
         for i, d in tqdm(enumerate(classes)):
@@ -52,16 +54,35 @@ class SoundLoader:
                 sampling_rate,
                 n_mels,
                 fft_window,
-                hop_length
+                hop_length,
+                epsilon
             )
             all_mels[d] = mels
         return all_mels
 
     @classmethod
-    def make_log_mels_from_root(cls, path, sampling_rate, n_mels, fft_window, hop_length, n_max=None):
+    def make_log_mels_from_root(cls, path, sampling_rate, n_mels, fft_window, hop_length, epsilon, n_max=None):
         assert os.path.isdir(path)
         classes = [d for d in os.listdir(path) if os.path.isdir(join(path, d))]
         if n_max is not None and len(classes) > n_classes:
             classes = classes[:n_max]
-        mels = cls.make_log_mels_for_classes(path, classes, sampling_rate, n_mels, fft_window, hop_length)
+        mels = cls.make_log_mels_for_classes(path, classes, sampling_rate, n_mels, fft_window, hop_length, epsilon)
+        return all_mels
+
+    @staticmethod
+    def save_mels(mels, dst):
+        if not os.path.exists(dst):
+            os.mkdir(dst)
+        for k, v in mels.items():
+            with open(join(dst, f'{k}.pkl'), 'wb') as f:
+                pickle.dump(v, f)
+
+    @staticmethod
+    def load_mels(src, classes):
+        all_mels = {}
+        for c in classes:
+            mel = pickle.load(
+                open(join(src, f'{c}.pkl'), 'rb')
+            )
+            all_mels[c] = mel
         return all_mels
